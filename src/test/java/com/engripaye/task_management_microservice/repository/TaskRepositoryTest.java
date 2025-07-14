18f1d91dc9300b9b680f23f3a46a2d304c464819
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-14 01:13:14 +0100

    updated commit by engr ipaye

--- src/test/java/com/engripaye/task_management_microservice/repository/TaskRepositoryTest.java
+++ src/test/java/com/engripaye/task_management_microservice/repository/TaskRepositoryTest.java
@@ -5,9 +5,12 @@ import com.engripaye.task_management_microservice.model.TaskStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,10 +18,20 @@ import static org.junit.jupiter.api.Assertions.*;
 @Testcontainers
 class TaskRepositoryTest {
     @Container
-    static PostgreSQLContainer<?> postgres= new PostgreSQLContainer<>("postgres:15")
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
             .withDatabaseName("taskdb")
             .withUsername("admin")
             .withPassword("secret");
+
+
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
 
     @Autowired
     private TaskRepository taskRepository;
