2d82ef772b23d5b851de61e2a3e3255b13c00439
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-14 01:38:43 +0100

    updated commit by engr ipaye

--- src/test/java/com/engripaye/task_management_microservice/repository/TaskRepositoryTest.java
+++ src/test/java/com/engripaye/task_management_microservice/repository/TaskRepositoryTest.java
@@ -1,52 +1,46 @@
 package com.engripaye.task_management_microservice.repository;
 
 import com.engripaye.task_management_microservice.model.Task;
-import com.engripaye.task_management_microservice.model.TaskStatus;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 @Testcontainers
-class TaskRepositoryTest {
+@SpringBootTest
+public class TaskRepositoryTest {
+
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
-            .withDatabaseName("taskdb")
+            .withDatabaseName("testdb")
             .withUsername("admin")
             .withPassword("secret");
 
-
-
     @DynamicPropertySource
-    static void overrideProperties(DynamicPropertyRegistry registry) {
+    static void configureTestProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
     }
 
-
     @Autowired
     private TaskRepository taskRepository;
 
-
     @Test
-    void testSaveAndFindTask() {
+    void testSaveTask() {
         Task task = new Task();
-        task.setId(1L);
-        task.setTitle("Test task");
-        task.setDescription("Test Description");
-        task.setStatus(TaskStatus.OPEN);
+        task.setTitle("Write tests");
+        task.setDescription("Write repository integration tests");
 
-        taskRepository.save(task);
+        Task savedTask = taskRepository.save(task);
 
-        assertTrue(taskRepository.findById(1L).isPresent());
+        assertThat(savedTask.getId()).isNotNull();
+        assertThat(savedTask.getTitle()).isEqualTo("Write tests");
     }
 }
