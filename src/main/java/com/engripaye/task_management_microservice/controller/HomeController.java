a716f7badecbf86c9b03d0ad8bbda8462a73d332
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-15 05:31:19 +0100

    updated commit for deployment by engr ipaye

--- src/main/java/com/engripaye/task_management_microservice/controller/HomeController.java
+++ src/main/java/com/engripaye/task_management_microservice/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.engripaye.task_management_microservice.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class HomeController{
 
     @GetMapping("/")
     public String home() {
-        return "\uD83D\uDE04\n Welcome To Task Management Service!";
+        return "\uD83D\uDE04\n Welcome to Task Management Microservice API!";
     }
 }
